26243819fbec78d33de3fb5d6d9c165b390c9f4a
HrantH-yan <hrant.harutyunyan6795@gmail.com>
2023-03-18 00:22:44 +0400

    fixed Store

--- RAM.cpp
+++ RAM.cpp
@@ -14,27 +14,29 @@ void RAM::Store(word address, word data)
     {
         throw std::runtime_error("Failed to open RAM file");
     }
-
     // copy all data of ram in temp and write it in ram(if not all this data will be set to zero)
-    if(address != 0)
+    ram.seekp(0,std::ios::end);
+    word ram_size = ram.tellp();
+    ram.seekg(0);
+    if(ram_size != 0)
     {
-        ram.seekp(address);
-        char * temp = new char[ram.tellp()];
-        if (!ram.read(temp, ram.tellp())) 
+        char * temp = new char[ram_size];
+        if (!ram.read(temp, ram_size)) 
         {
+            delete temp;
             throw std::runtime_error("Failed to store data in RAM file");
         }
-        if (!ram.write(temp,ram.tellp())) 
+        ram.seekp(0);
+        if (!ram.write(temp,ram_size)) 
         {
+            delete temp;
             throw std::runtime_error("Failed to store data in RAM file");
         }
         delete temp;
-        ram.seekp(0);
     }
-
+    ram.seekp(address);
     for(int i = 0; i < sizeof(word); ++i)
     {
-        ram.seekp(address + i);
         byte = ((data>>((sizeof(word)-(i+1))*8)) & 0xff);
 
         if (!ram.write (&byte, 1)) 
